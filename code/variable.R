7257b427fa17ae0f3dacf18ebce4b86296f2f081
Ngu Cheng Jie <95898617+BensonNgu@users.noreply.github.com>
2025-09-14 05:50:40 +0000

    week 1 - section 1

---
+++ code/variable.R
@@ -0,0 +1,42 @@
+# Read input (not needed for this challenge)
+
+# TODO: Write your code below
+x <- 10
+y <- 5
+
+# Perform arithmetic operations
+# (Add your code here)
+addition <- x + y
+subtraction <- x - y
+multiplication <- x * y
+division <- x / y
+
+
+# Print the results
+# (Add your code here)
+cat("x + y =", addition, "\n")
+cat("x - y =", subtraction, "\n")
+cat("x * y =", multiplication, "\n")
+cat("x / y =", division, "\n")
+# Remember to format your output exactly as shown in the challenge description
+
+# Defining character
+name <- 'benson'
+description <- "A fresh graduate who inspring full stack development"
+# characters can be enclosed in single or double quotes
+
+# Defining an INTEGER
+num1 <- 5L  # The 'L' suffix explicitly declares an integer
+
+# Defining a double 
+num2 <- 5.1
+
+# How to check data types
+class(num1)
+# Returns "integer"
+
+class(num2)
+# Returns "numeric"
+
+class(name)
+# Returns "character"
